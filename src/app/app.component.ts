8b9e6d982cc91a80741c00abd7056699506dd2c9
Jonas Mahlburg <darrylmanga@yahoo.com>
2024-07-31 16:44:56 +0200

    added Footer and skilltree (Version 1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { WhyMeComponent } from './why-me/why-me.component';
 import { SkillsComponent } from "./skills/skills.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 
 
@@ -12,7 +13,7 @@ import { ContactComponent } from "./contact/contact.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, WhyMeComponent, SkillsComponent, ProjectsComponent, ContactComponent],
+  imports: [RouterOutlet, LandingPageComponent, WhyMeComponent, SkillsComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

b2234709761043fe4b8a8a24565719fb25e0cc4d
Jonas Mahlburg <darrylmanga@yahoo.com>
2024-07-31 09:24:27 +0200

    rearange code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { landingPageComponent } from "./landingPage/landingPage.component";
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { WhyMeComponent } from './why-me/why-me.component';
+import { SkillsComponent } from "./skills/skills.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ContactComponent } from "./contact/contact.component";
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, landingPageComponent, WhyMeComponent],
+  imports: [RouterOutlet, LandingPageComponent, WhyMeComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

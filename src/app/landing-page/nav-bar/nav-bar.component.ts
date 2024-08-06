43d6d8ff94c1e11a968fb4c588154c36fcfc79aa
Jonas Mahlburg <darrylmanga@yahoo.com>
2024-08-06 08:19:52 +0200

    improve some code & added contact form

--- src/app/landing-page/nav-bar/nav-bar.component.ts
+++ src/app/landing-page/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,6 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  @ViewChild('myElement') myElement!: ElementRef;
 
+
+  showCoords(event: MouseEvent) {
+    let x = event.clientX;
+    let y = event.clientY;
+    let text = "X coords: " + x + ", Y coords: " + y;
+    // document.getElementById("demo").innerHTML = text;
+    // console.log(text);
+    // if (x >= 879 && x <= 898) {
+    //   console.log("x is between 879 and 898");
+    //   document.getElementById('language').
+    // } else {
+    //   console.log("x is not between 879 and 898");
+    // }
+  }
 }
 

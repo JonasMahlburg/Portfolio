2f4ae2983836e13a479102518e51994f7a833ab7
Jonas Mahlburg <darrylmanga@yahoo.com>
2024-08-08 07:47:20 +0200

    added stylechanges

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'darrylmanga@yahoo.com';  
+            $recipient = 'mail@jonas-mahlburg.de  ';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     

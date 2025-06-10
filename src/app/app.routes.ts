5acadacdb449756f14011e8e02fcd9b1ea2b4534
JonasMahlburg <darrylmanga@yahoo.com>
2025-06-10 18:01:48 +0200

    feat: connect error page to routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import {  Routes} from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { MainContentComponent } from './main-content/main-content.component';
-
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 export const routes: Routes = [
   { path: '', component: MainContentComponent },
   { path: 'Imprint', component: ImprintComponent },
-  { path: 'PrivacyPolicy', component: PrivacyPolicyComponent }
+  { path: 'PrivacyPolicy', component: PrivacyPolicyComponent },
+  { path: 'error', component: ErrorPageComponent },
+  { path: '**', component: ErrorPageComponent }
 ];
-
-

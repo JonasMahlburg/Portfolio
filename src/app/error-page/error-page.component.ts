098c4326d2a29925dcfde807c4d89fbf3e38a6e5
JonasMahlburg <darrylmanga@yahoo.com>
2025-06-11 08:03:52 +0200

    refactor: 404 error Page style: outsourcing of CSS code

--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../translation.service';
 
 @Component({
   selector: 'app-error-page',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './error-page.component.html',
   styleUrl: './error-page.component.scss'
 })
 export class ErrorPageComponent {
-
+  translate = inject(TranslationService);
 }

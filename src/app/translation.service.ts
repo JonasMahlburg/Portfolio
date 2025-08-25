f419362a42f809407287abf02b145962b6cddbd8
JonasMahlburg <darrylmanga@yahoo.com>
2025-08-25 16:41:40 +0200

    feat: save language in local storage, added some webkit-specific css and update landing-page

--- src/app/translation.service.ts
+++ src/app/translation.service.ts
@@ -1,3 +1,20 @@
+// import { Injectable } from '@angular/core';
+// import { TranslateService } from '@ngx-translate/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class TranslationService {
+
+//   constructor(private translate: TranslateService) {
+//     translate.setDefaultLang('en');
+//    }
+
+//    public switchLanguage(language: string): void{
+//     this.translate.use(language);
+//    }
+// }
+
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -7,10 +24,21 @@ import { TranslateService } from '@ngx-translate/core';
 export class TranslationService {
 
   constructor(private translate: TranslateService) {
-    translate.setDefaultLang('en');
-   }
+    // 1. Lade die Sprache beim Start des Services
+    const storedLang = localStorage.getItem('preferredLanguage');
+    if (storedLang) {
+      this.translate.use(storedLang);
+    } else {
+      // Setze eine Standard-Sprache, falls noch keine gespeichert ist
+      this.translate.setDefaultLang('en');
+    }
+  }
 
-   public switchLanguage(language: string): void{
+  public switchLanguage(language: string): void {
+    // 2. Wechsle die Sprache in der App
     this.translate.use(language);
-   }
+    
+    // 3. Speichere die gewählte Sprache im Local Storage
+    localStorage.setItem('preferredLanguage', language);
+  }
 }
